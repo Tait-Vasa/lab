bf2a6258baddcaabecc9ce2cd10aee77599cc7bc
Tait <taiterv@gmail.com>
2022-11-18 21:10:10 -0600

    inital commit

--- account.py
+++ account.py
@@ -1,10 +1,18 @@
 class Account:
-    def __init__(self, name):
-        # this is the
+    def __init__(self, name : str) -> None :
+        '''
+        This is init function
+        :param name of the account
+        '''
         self.__name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount : float) -> bool:
+        '''
+        This is the deposit function add money to an account
+        :param amount: this is amount selected to be put in
+        :return: returns if the function deposited or not
+        '''
 
         if amount > 0:
             self.__account_balance += amount
@@ -13,7 +21,12 @@ class Account:
             return False
 
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        '''
+        This is the deposit function add money to an account
+        :param amount: this is amount selected to be taken out
+        :return: returns if the function withdrew or not
+        '''
         if amount > 0:
             self.__account_balance -= amount
             return True
@@ -21,8 +34,16 @@ class Account:
             return False
 
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        '''
+        This displays the account balance.
+        :return: returns the amount in the account
+        '''
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        '''
+        This function displays the name of the account.
+        :return: returns the account name
+        '''
         return self.__name
---
+++ test_account.py
@@ -0,0 +1,43 @@
+import pytest
+from account import *
+class Test:
+    def setup_method(self):
+        self.p1 = Account('Tait')
+
+    def teardown_method(self):
+        del self.p1
+
+    def test_init(self):
+        assert self.p1.get_name() == "Tait"
+        assert self.p1.get_balance() == 0
+
+
+    def test_deposit(self):
+
+        assert self.p1.deposit(30) is True
+        assert self.p1.get_balance() == 30
+
+        assert self.p1.deposit(-30) is False
+        assert self.p1.get_balance() == 30
+
+        assert self.p1.deposit(0) is False
+        assert self.p1.get_balance() == 30
+
+    def test_withdraw(self):
+        assert self.p1.withdraw(15) is False
+        assert self.p1.get_balance() == 0
+
+        assert self.p1.deposit(30) is True
+        assert self.p1.get_balance() == 30
+
+        assert self.p1.withdraw(15) is True
+        assert self.p1.get_balance() == 15
+
+        assert self.p1.withdraw(25) is False
+        assert self.p1.get_balance() == 15
+
+        assert self.p1.withdraw(-15) is False
+        assert self.p1.get_balance() == 15
+
+        assert self.p1.withdraw(0) is False
+        assert self.p1.get_balance() == 15
